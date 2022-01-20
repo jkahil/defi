e5f13a16f6801b83e0233574c7d8b03d778e830e
joelk <joel.kahil@gmail.com>
2022-01-20 20:29:15 +0000

    Aave Library updated to retrieve positions of a list of accounts

--- aave.py
+++ aave.py
@@ -1,10 +1,14 @@
 from utils import client,todayTimestamp
 from gql import gql
+import json
 from utils import TIMESTAMP_PER_YEAR
 import pandas as pd
 import numpy as np
 
 AAVE_API="https://api.thegraph.com/subgraphs/name/aave/protocol-v2"
+
+def lookDecimals(pool,symbol):
+    return pool.loc[symbol]['decimals']
 
 class Aave:
     def __init__(self,url=AAVE_API):
@@ -15,6 +19,7 @@ class Aave:
             id
         underlyingAsset
         symbol
+        decimals
         }
         }'''
         cli = self.client
@@ -26,6 +31,7 @@ class Aave:
     def currentUsers(self,symbol):
         cli=self.client
         reserveid=self.pools.loc[symbol]['id']
+        decimals=self.pools.loc[symbol]['decimals']
         lastid=''
         query=''' query($reserve_id: ID!,$lastID:ID!){
             userReserves(first: 1000,
@@ -57,19 +63,96 @@ class Aave:
                     "lastID": lastid,
                 }
                 res = cli.execute(gql(query),variable_values=params)
-                res=pd.json_normalize(res['userReserves'])
+                res=pd.json_normalize(res['userReserves']).drop('reserve.id',axis=1)
+                # Rebase all the columns to decimals
+                col_to_rebase=[col for col in res.columns if col[:3]=='cur']
+                res[col_to_rebase]=res[col_to_rebase].astype('float')
+                res[col_to_rebase]=res[col_to_rebase]/10**decimals
                 if len(res) == 0:
                     return agg
                 else:
                     res.index = res.id
                     lastid = res.iloc[-1].id  # .values
-                    print(lastid)
                     res = res.drop('id', axis=1)
+                    res=res.set_index('user.id')
                     agg = agg.append(res)
             except:
                 return agg
         return agg
 
+    def userPosition(self,userids):
+        """
+
+        :param self:
+        :param userids: list of userids we want to analyse
+        :return:all the user positions broken down by token
+        """
+        lastid = ''
+        cli=self.client
+        finished=0
+        agg = pd.DataFrame()
+        query=''' query($user_list:[ID!]!,$lastID:ID!){
+            userReserves(first: 1000, where: {user_in: $user_list,user_gt:$lastID})
+        {
+
+            id
+        reserve
+        {
+            id
+        symbol
+        }
+        user
+        {
+            id
+        }
+        currentATokenBalance
+        scaledATokenBalance
+        currentStableDebt
+        currentVariableDebt
+        currentTotalDebt
+        }
+        }'''
+        while (finished == 0):
+            try:
+                params = {
+                    "user_list": list(userids),
+                    "lastID":lastid
+                }
+                res = cli.execute(gql(query), variable_values=params)
+                res = pd.json_normalize(res['userReserves']).drop('reserve.id', axis=1)
+                res['Decimals'] = res.apply(lambda x: lookDecimals(self.pools, x['reserve.symbol']),axis=1)
+                # Rebase all the columns to decimals
+                col_to_rebase = [col for col in res.columns if col[:3] == 'cur']
+                for c in col_to_rebase:
+                    res[c] = res[c].astype('float')
+                    res[c] = res[c] / 10 ** res['Decimals']
+                if len(res) == 0:
+                    return agg
+                else:
+                    res.index = res.id
+                    lastid = res.iloc[-1].id  # .values
+                    res = res.drop('id', axis=1)
+                    res = res.set_index('user.id')
+                    agg = agg.append(res)
+            except:
+                return agg
+        return agg
+
+    def getPositions(self, ids, steps=100):
+        """
+        Need to cut the pool in small bits to retrieve all the information
+        Args:
+            ids: list of id we want to get all the positions
+            steps: how many accounts to retrieve data simultaneously
+
+        """
+        nb_gp = int(np.ceil(len(ids) / steps))
+        groups = [ids[(i) * steps:(i + 1) * steps] for i in range(0, nb_gp)]
+        return pd.concat([self.userPosition(gp) for gp in groups])
+
+"""
 pool=Aave()
 userdata=pool.currentUsers('DAI')
+pos=pool.userPosition(userdata.index[0:100])
 print('finished')
+"""
